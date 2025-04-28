9b6542211c59483035d184bb1bcbd87b24c42351
Bloodssa <154530573+Bloodssa@users.noreply.github.com>
2025-04-28 19:22:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function myChangeOne() {
+    document.getElementById('stream_img').src="https://www.pngplay.com/wp-content/uploads/10/Napoleon-Bonaparte-Transparent-PNG.png";
+    document.getElementById('name').innerHTML = "Napoleon-Bonaparte";
+}
+function myChangeTwo() {
+    document.getElementById('stream_img').src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Julius_Caesar_Coustou_Louvre.png/1200px-Julius_Caesar_Coustou_Louvre.png";
+    document.getElementById('name').innerHTML = "Julius Caesar";
+}
+function myChangeThree() {
+    document.getElementById('stream_img').src="https://pngimg.com/d/mark_zuckerberg_PNG22.png";
+    document.getElementById('name').innerHTML = "Mark Zuckerberg";
+}
